2727ebabb079d32fd3d52aaedb36b13a8c16f36f
siva9492836828 <84285697+siva9492836828@users.noreply.github.com>
2021-06-30 09:59:37 +0530

    Update settings.py

--- a/settings.py
+++ a/settings.py
@@ -124,11 +124,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[
+ STATIC_DIR,
+ ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
